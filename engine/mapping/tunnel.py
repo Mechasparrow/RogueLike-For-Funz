1a9d31c2a49a1f2c8f8fb975f355134816bd260d
Mechasparrow <mikipux7@gmail.com>
2018-12-03 16:14:10 -0600

    organized tunnel code structure

--- engine/mapping/tunnel.py
+++ engine/mapping/tunnel.py
@@ -10,111 +10,115 @@ class Tunnel:
 
 
     def draw_tunnel(self, map):
-
         if (self.orientation == "h"):
-
-            dx = abs(self.point2[0] - self.point1[0])
-            dy = (self.point2[1] - self.point1[1])
-
-
-            mid_way = self.point1[0] + (dx // 2)
-
-            y = self.point1[1]
-
-
-            for x in range(self.point1[0], self.point2[0]):
-                # Draw walls for tunnel
-                if (x != self.point1[0] and x != (self.point2[0] - 1)):
-                    map[x][y-1] = Tile(x, y-1, blocking = True)
-                    map[x][y+1] = Tile(x, y+1, blocking = True)
-
-                map[x][y] = Tile(x, y, walkable = True)
-
-
-                if (x == mid_way):
-
-                    if (dy > 0):
-                        # Fix special edges
-                        map[x+1][y] = Tile(x+1, y, blocking = True)
-                        map[x+1][y-1] = Tile(x+1, y-1, blocking = True)
-                        map[x][y+dy+1] = Tile(x, y+dy+1, blocking = True)
-                        map[x-1][y+dy+1] = Tile(x-1, y+dy+1, blocking = True)
-                    elif (dy < 0):
-                        map[x+1][y] = Tile(x+1, y, blocking = True)
-                        map[x+1][y+1] = Tile(x+1, y+1, blocking = True)
-                        map[x][y+dy-1] = Tile(x, y+dy-1, blocking = True)
-                        map[x-1][y+dy-1] = Tile(x-1, y+dy-1, blocking = True)
-
-                    for i in range(0, abs(dy)):
-                        if (dy > 0):
-                            y += 1
-
-                            if (y >= self.point1[1] and y <= (self.point1[1] + dy)):
-                                map[x+1][y] = Tile(x+1, y, blocking = True)
-                                map[x-1][y] = Tile(x-1, y, blocking = True)
-
-                        if (dy < 0):
-                            y -= 1
-
-                            if (y <= self.point1[1] and y >= (self.point1[1] + dy)):
-                                map[x+1][y] = Tile(x+1, y, blocking = True)
-                                map[x-1][y] = Tile(x-1, y, blocking = True)
-
-                        # if (y == (self.point[1] + dy)):
-
-                        map[x][y] = Tile(x, y, walkable = True)
+            self.draw_h_tunnel(map)
 
         elif (self.orientation == "v"):
-            dx = (self.point2[0] - self.point1[0])
-            print (dx)
+            self.draw_v_tunnel(map)
 
-            dy = abs(self.point2[1] - self.point1[1])
+    def draw_v_tunnel(self,map):
+        dx = (self.point2[0] - self.point1[0])
+        print (dx)
 
-            x = self.point1[0]
+        dy = abs(self.point2[1] - self.point1[1])
 
-            mid_way = self.point1[1] + (dy // 2)
+        x = self.point1[0]
 
-            for y in range(self.point1[1], self.point2[1]):
+        mid_way = self.point1[1] + (dy // 2)
 
-                if (y != self.point1[1] and y != (self.point2[1] - 1)):
-                    map[x+1][y] = Tile(x+1, y, blocking = True)
-                    map[x-1][y] = Tile(x-1, y, blocking = True)
+        for y in range(self.point1[1], self.point2[1]):
 
-                map[x][y] = Tile(x, y, walkable = True)
+            if (y != self.point1[1] and y != (self.point2[1] - 1)):
+                map[x+1][y] = Tile(x+1, y, blocking = True)
+                map[x-1][y] = Tile(x-1, y, blocking = True)
 
-                if (y == mid_way):
+            map[x][y] = Tile(x, y, walkable = True)
 
-                    # Edge cases
+            if (y == mid_way):
+
+                # Edge cases
+
+                if (dx > 0):
+                    map[x-1][y+1] = Tile(x-1, y+1, blocking =True)
+                    map[x][y+1] = Tile(x, y+1, blocking =True)
+                    map[x+dx+1][y-1] = Tile(x+dx+1, y-1, blocking =True)
+                    map[x+dx+1][y] = Tile(x+dx+1, y, blocking =True)
+                elif (dx < 0):
+                    map[x][y+1] = Tile(x, y+1, blocking = True)
+                    map[x+1][y+1] = Tile(x+1, y+1, blocking = True)
+                    map[x+dx-1][y] = Tile(x+dx-1, y, blocking = True)
+                    map[x+dx-1][y-1] = Tile(x+dx-1, y-1, blocking = True)
+
+
+                for i in range(0, abs(dx)):
+
+
 
                     if (dx > 0):
-                        map[x-1][y+1] = Tile(x-1, y+1, blocking =True)
-                        map[x][y+1] = Tile(x, y+1, blocking =True)
-                        map[x+dx+1][y-1] = Tile(x+dx+1, y-1, blocking =True)
-                        map[x+dx+1][y] = Tile(x+dx+1, y, blocking =True)
-                    elif (dx < 0):
-                        map[x][y+1] = Tile(x, y+1, blocking = True)
-                        map[x+1][y+1] = Tile(x+1, y+1, blocking = True)
-                        map[x+dx-1][y] = Tile(x+dx-1, y, blocking = True)
-                        map[x+dx-1][y-1] = Tile(x+dx-1, y-1, blocking = True)
+                        x += 1
+
+                        if (x > self.point1[0] and x <= self.point2[0]):
+                            map[x][y+1] = Tile(x, y+1, blocking = True)
+                            map[x][y-1] = Tile(x, y-1, blocking = True)
+
+                    if (dx < 0):
+                        x -= 1
+
+                        if (x < self.point1[0] and x >= self.point2[0]):
+                            map[x][y+1] = Tile(x, y+1, blocking = True)
+                            map[x][y-1] = Tile(x, y-1, blocking = True)
 
 
-                    for i in range(0, abs(dx)):
+                    map[x][y] = Tile(x, y, walkable = True)
+
+    def draw_h_tunnel(self,map):
+        dx = abs(self.point2[0] - self.point1[0])
+        dy = (self.point2[1] - self.point1[1])
 
 
+        mid_way = self.point1[0] + (dx // 2)
 
-                        if (dx > 0):
-                            x += 1
-
-                            if (x > self.point1[0] and x <= self.point2[0]):
-                                map[x][y+1] = Tile(x, y+1, blocking = True)
-                                map[x][y-1] = Tile(x, y-1, blocking = True)
-
-                        if (dx < 0):
-                            x -= 1
-
-                            if (x < self.point1[0] and x >= self.point2[0]):
-                                map[x][y+1] = Tile(x, y+1, blocking = True)
-                                map[x][y-1] = Tile(x, y-1, blocking = True)
+        y = self.point1[1]
 
 
-                        map[x][y] = Tile(x, y, walkable = True)
+        for x in range(self.point1[0], self.point2[0]):
+            # Draw walls for tunnel
+            if (x != self.point1[0] and x != (self.point2[0] - 1)):
+                map[x][y-1] = Tile(x, y-1, blocking = True)
+                map[x][y+1] = Tile(x, y+1, blocking = True)
+
+            map[x][y] = Tile(x, y, walkable = True)
+
+
+            if (x == mid_way):
+
+                if (dy > 0):
+                    # Fix special edges
+                    map[x+1][y] = Tile(x+1, y, blocking = True)
+                    map[x+1][y-1] = Tile(x+1, y-1, blocking = True)
+                    map[x][y+dy+1] = Tile(x, y+dy+1, blocking = True)
+                    map[x-1][y+dy+1] = Tile(x-1, y+dy+1, blocking = True)
+                elif (dy < 0):
+                    map[x+1][y] = Tile(x+1, y, blocking = True)
+                    map[x+1][y+1] = Tile(x+1, y+1, blocking = True)
+                    map[x][y+dy-1] = Tile(x, y+dy-1, blocking = True)
+                    map[x-1][y+dy-1] = Tile(x-1, y+dy-1, blocking = True)
+
+                for i in range(0, abs(dy)):
+                    if (dy > 0):
+                        y += 1
+
+                        if (y >= self.point1[1] and y <= (self.point1[1] + dy)):
+                            map[x+1][y] = Tile(x+1, y, blocking = True)
+                            map[x-1][y] = Tile(x-1, y, blocking = True)
+
+                    if (dy < 0):
+                        y -= 1
+
+                        if (y <= self.point1[1] and y >= (self.point1[1] + dy)):
+                            map[x+1][y] = Tile(x+1, y, blocking = True)
+                            map[x-1][y] = Tile(x-1, y, blocking = True)
+
+                    # if (y == (self.point[1] + dy)):
+
+                    map[x][y] = Tile(x, y, walkable = True)
